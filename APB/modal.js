7f00f59bfccb22678553b6a4e99c4ee4a8757e8f
teport <123849941+teport@users.noreply.github.com>
2024-10-10 09:29:08 -0600

    Update modal.js

--- APB/modal.js
+++ APB/modal.js
@@ -1,23 +1,22 @@
-// Muestra la ventana emergente después de que la página se haya cargado
+
 window.onload = function() {
-    // Obtén el modal y el botón de cerrar
+
     const modal = document.getElementById('modal');
     const closeSpan = document.querySelector('.close');
 
-    // Configura el retraso en milisegundos (5000 ms = 5 segundos)
-    const delay = 800; // 5000 ms = 5 segundos
+    const delay = 800;
 
-    // Muestra el modal después del retraso
+
     setTimeout(function() {
         modal.style.display = 'block';
     }, delay);
 
-    // Cierra el modal cuando se hace clic en el botón de cerrar
+
     closeSpan.onclick = function() {
         modal.style.display = 'none';
     };
 
-    // Cierra el modal si el usuario hace clic fuera del contenido del modal
+
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = 'none';
